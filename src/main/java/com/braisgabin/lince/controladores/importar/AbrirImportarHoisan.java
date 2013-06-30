c6fdd7d2e67f8855d21921ed6862e200240a3e41
Brais Gabin <braisgabin@gmail.com>
2013-06-30 16:11:51 +0200

    Import registers

--- src/main/java/com/braisgabin/lince/controladores/importar/AbrirImportarHoisan.java
+++ src/main/java/com/braisgabin/lince/controladores/importar/AbrirImportarHoisan.java
@@ -18,6 +18,7 @@
 package com.braisgabin.lince.controladores.importar;
 
 import com.braisgabin.lince.Command;
+import com.braisgabin.lince.modelo.Registro;
 import com.braisgabin.lince.modelo.instrumentoobservacional.*;
 import com.braisgabin.lince.utiles.FiltroArchivos;
 import com.braisgabin.lince.utiles.PathArchivos;
@@ -26,14 +27,15 @@ import com.healthmarketscience.jackcess.Table;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.Action;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
 
 /**
  *
@@ -59,12 +61,36 @@ public class AbrirImportarHoisan extends Command {
                 Table criterios = db.getTable("Criterios");
                 Table categorias = db.getTable("Categorias");
                 InstrumentoObservacional instrumentoObservacional = generateInstrumentObservacional(criterios, categorias);
+                Table tiempos = db.getTable("TiemposObservaciones");
+                Table tiempoCategorias = db.getTable("TiemposCategoriasObservadas");
+                Map<String, Registro> registros = generateRegistros(tiempos, tiempoCategorias, criterios, categorias, instrumentoObservacional);
 
+                File parentFile = f.getParentFile();
+                File linceDir = getFreeFile(parentFile, "Lince", "");
+                linceDir.mkdir();
+
+                instrumentoObservacional.setPath(getFreeFile(linceDir, f.getName(), ".ilince"));
+                instrumentoObservacional.save();
+
+                for (Entry<String, Registro> row : registros.entrySet()) {
+                    Registro registro = row.getValue();
+                    registro.setPath(getFreeFile(linceDir, new File(row.getKey()).getName(), ".rlince"));
+                    registro.save();
+                }
 
             } catch (IOException ex) {
                 Logger.getLogger(AbrirImportarHoisan.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+
+    private File getFreeFile(File dir, String name, String extension) {
+        File f = new File(dir, name + extension);
+        int i = 2;
+        while (f.exists()) {
+            f = new File(dir, name + " (" + i++ + ")" + extension);
+        }
+        return f;
     }
 
     @Override
@@ -98,18 +124,81 @@ public class AbrirImportarHoisan extends Command {
                 // Ignore
             }
         }
-        return null;
+        return instrumentoObservacional;
     }
 
     private void generateCategorias(Criterio criterio, Integer creterioId, Table categorias) {
         for (Map<String, Object> row : categorias) {
             if (row.get("ID_Criterio_Referencial").equals(creterioId)) {
                 InstrumentoObservacional.getInstance().addHijo(criterio, (String) row.get("Descripcion"));
-                
+
                 Categoria categoria = (Categoria) criterio.getChildAt(criterio.getChildCount() - 1);
                 categoria.setCodigo((String) row.get("Nombre_Categoria"));
                 categoria.setDescripcion((String) row.get("Nucleo_Categorial"));
             }
         }
+    }
+
+    private Map<String, Registro> generateRegistros(Table tiempos, Table tiempoCategorias, Table criterios, Table categorias, InstrumentoObservacional instrumentoObservacional) {
+        Map<String, Registro> registros = new HashMap<String, Registro>();
+        for (Map<String, Object> row : tiempos) {
+            String nombre = (String) row.get("Nombre_Video");
+            Registro registro = registros.get(nombre);
+            if (registros.get(nombre) == null) {
+                Registro.loadNewInstance();
+                registro = Registro.getInstance();
+                registros.put(nombre, registro);
+            }
+            registro.addRow(((Integer) row.get("TFramesInicial")) * 40, generateRegisterRow((Integer) row.get("ID_Tiempo"), tiempoCategorias, criterios, categorias, instrumentoObservacional.getCriterios()), new HashMap<NodoInformacion, String>());
+        }
+        return registros;
+    }
+
+    private Map<Criterio, Categoria> generateRegisterRow(Integer id, Table tiempoCategorias, Table criterioTable, Table categoriaTable, Criterio criterios[]) {
+        Map<Criterio, Categoria> r = new HashMap<Criterio, Categoria>();
+        for (Map<String, Object> row : tiempoCategorias) {
+            if (row.get("ID_Tiempo").equals(id)) {
+                Criterio c = findCriterio(criterios, criterioTable, (Integer) row.get("ID_Criterio"));
+                if (c != null) { // TODO registrar los fijos
+                    Categoria ca = findCategoria(c, categoriaTable, (Integer) row.get("ID_Categoria"));
+                    r.put(c, ca);
+                }
+            }
+        }
+        return r;
+    }
+
+    private Criterio findCriterio(Criterio cs[], Table criterios, int id) {
+        String nombre = findCreterioCode(criterios, id);
+        for (Criterio c : cs) {
+            if (c.getNombre().equals(nombre)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    private String findCreterioCode(Table criterioTable, int id) {
+        for (Map<String, Object> row : criterioTable) {
+            if (row.get("ID_Criterio").equals(id)) {
+                return (String) row.get("Nombre_Criterio");
+            }
+        }
+        return null;
+    }
+
+    private Categoria findCategoria(Criterio c, Table categoriaTable, Integer id) {
+        String code = findCategoriaCode(categoriaTable, id);
+
+        return c.getCategoriaByCodigo(code);
+    }
+
+    private String findCategoriaCode(Table categoriaTable, Integer id) {
+        for (Map<String, Object> row : categoriaTable) {
+            if (row.get("ID_Categoria").equals(id)) {
+                return (String) row.get("Nombre_Categoria");
+            }
+        }
+        return null;
     }
 }
